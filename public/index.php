650a3eaa1cf7e9fce873129a6514b43d4d579a56
faikarghas <ghassanfaikar@yahoo.com>
2021-05-15 00:32:42 +0700

    first init

--- public/index.php
+++ public/index.php
@@ -31,7 +31,7 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 |
 */
 
-require __DIR__.'/../adminadvisor/vendor/autoload.php';
+require __DIR__.'/adminadvisor/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ require __DIR__.'/../adminadvisor/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../adminadvisor/bootstrap/app.php';
+$app = require_once __DIR__.'/adminadvisor/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
