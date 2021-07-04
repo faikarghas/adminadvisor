2feb4a0a61501b68e1bce2cf09d5ba7a97bfcd3b
Faikar <ghassanfaikar@yahoo.com>
2021-07-05 02:32:11 +0700

    fellow

--- resources/views/v_fellows.blade.php
+++ resources/views/v_fellows.blade.php
@@ -128,7 +128,7 @@
                         <div class="card-body">
                           <ul class="nav nav-tabs" id="myTab" role="tablist">
                             <li class="nav-item">
-                              <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Fellow</a>
+                              <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Fellows</a>
                             </li>
                             <li class="nav-item">
                               <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Accepted</a>
